91fb31fbf80d1494d5f77b1f4ce965d28f856c02
Krishna Singh <singhkrishna93244@gmail.com>
2025-07-20 21:11:35 +0530

    main file changes

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from routes.order_routes import router as OrderRouter
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
+@app.get("/", include_in_schema=False)
+def root():
     return {"message": "HROne Backend is running!"}
 
 app.include_router(ProductRouter, prefix="/products", tags=["Products"])
